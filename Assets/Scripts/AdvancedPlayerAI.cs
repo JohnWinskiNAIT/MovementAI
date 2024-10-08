f4b4aeff9c8e4a3336eed6f361b7c91ba96b5765
John Winski <JWINSKI@nait.ca>
2024-10-08 16:37:11 -0600

    Separated the OnTriggerEnter to the detector script.

--- Assets/Scripts/AdvancedPlayerAI.cs
+++ Assets/Scripts/AdvancedPlayerAI.cs
@@ -26,43 +26,37 @@ public class AdvancedPlayerAI : MonoBehaviour
         transform.Translate(Vector3.forward * Time.fixedDeltaTime * movementSpeed);
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void TurnPlayer()
     {
-        // If the front trigger detects an object
-        if (other.gameObject.tag == "Wall")
+        // If the object in front of the AI is on the wall layer
+        if (Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.forward, out hit, Quaternion.identity, 1.5f, myMask))
         {
-            // If the object in front of the AI is on the wall layer
-            if (Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.forward, out hit, Quaternion.identity, 1.5f, myMask))
+            transform.LookAt(transform.position - hit.normal);
+
+            // Look left and right for walls
+            leftCast = Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), -transform.right, Quaternion.identity, 8.0f, myMask);
+            rightCast = Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.right, Quaternion.identity, 8.0f, myMask);
+
+            if (!rightCast && leftCast)
             {
-                transform.LookAt(transform.position - hit.normal);
-
-                // Look left and right for walls
-                leftCast = Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), -transform.right, Quaternion.identity, 8.0f, myMask);
-                rightCast = Physics.BoxCast(playerView.transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.right, Quaternion.identity, 8.0f, myMask);
-
-                if (!rightCast && leftCast)
-                {
-                    transform.Rotate(Vector3.up, 90);
-                }
-                else if (rightCast && !leftCast)
-                {
-                    transform.Rotate(Vector3.up, -90);
-                }
-                else if (leftCast && rightCast)
-                {
-                    transform.Rotate(Vector3.up, 180);
-                }
-                else
-                {
-                    transform.Rotate(Vector3.up, 90);
-                }
+                transform.Rotate(Vector3.up, 90);
+            }
+            else if (rightCast && !leftCast)
+            {
+                transform.Rotate(Vector3.up, -90);
+            }
+            else if (leftCast && rightCast)
+            {
+                transform.Rotate(Vector3.up, 180);
             }
             else
             {
-                rbody.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
+                transform.Rotate(Vector3.up, 90);
             }
-
-
+        }
+        else
+        {
+            rbody.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
         }
     }
 }
