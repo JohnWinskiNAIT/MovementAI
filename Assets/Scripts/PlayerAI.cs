7a95f03cf17ad8b8b663460896d7ad53703c5450
John Winski <JWINSKI@nait.ca>
2024-10-08 14:54:07 -0600

    Cleaned up code

--- Assets/Scripts/PlayerAI.cs
+++ Assets/Scripts/PlayerAI.cs
@@ -7,7 +7,7 @@ public class PlayerAI : MonoBehaviour
     [SerializeField] float movementSpeed = 3.0f;
     Rigidbody rbody;
     [SerializeField] GameObject detector;
-    [SerializeField] float forceAmount = 10f;
+    [SerializeField] float forceAmount = 7f;
     [SerializeField] LayerMask myMask;
     
     // Start is called before the first frame update
@@ -16,28 +16,9 @@ public class PlayerAI : MonoBehaviour
         rbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
         transform.Translate(Vector3.forward * Time.fixedDeltaTime * movementSpeed);
-
-        //if (rbody != null)
-        //{
-        //    if (rbody.velocity.magnitude == 0)
-        //    {
-        //        transform.Rotate(Vector3.up, 180);
-        //    }
-        //}
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-                
     }
 
     private void OnTriggerEnter(Collider other)
@@ -45,14 +26,12 @@ public class PlayerAI : MonoBehaviour
         if (other.gameObject.tag == "Wall")
         {
             Debug.Log("Wall");
-            if (Physics.BoxCast(detector.transform.position, new Vector3(0.5f, 1, 0.5f), transform.forward, Quaternion.identity, 1.5f, myMask) || Physics.CheckBox(detector.transform.position, new Vector3(0.5f, 1, 0.4f),Quaternion.identity, myMask))
-            //if (Physics.Raycast(detector.transform.position, transform.forward, 1.5f))
+            if (Physics.BoxCast(transform.position, new Vector3(0.5f, 0.5f, 0.5f), transform.forward, Quaternion.identity, 1.5f, myMask))
             {
                 transform.Rotate(Vector3.up, 90);
             }
             else
             {
-                Debug.Log("Jump");
                 rbody.AddForce(Vector3.up * forceAmount, ForceMode.Impulse);
             }
             
